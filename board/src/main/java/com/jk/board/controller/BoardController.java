a7d095b936570810116e332e6a2daf2a18ccf3e2
Junku Kim <kimjunku0320@gmail.com>
2023-09-12 18:41:37 +0900

    DB에 테스트 데이터 넣고 모든 데이터 화면에 띄우기

--- board/src/main/java/com/jk/board/controller/BoardController.java
+++ board/src/main/java/com/jk/board/controller/BoardController.java
@@ -1,11 +1,13 @@
 package com.jk.board.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.jk.board.domain.Board;
 import com.jk.board.service.BoardService;
+
 
 @Controller
 public class BoardController {
@@ -22,11 +24,23 @@ public class BoardController {
 		return "boardwrite";
 	}
 	
+	/*
+	 * rediect:를 하면 브라우저에서 페이지를 새로 고치고 새 URL로 이동하게 된다.
+	 * 하지 않는 다면 브라우저 주소 표시줄에는 현재 요청한 URL이 남아있고 페이지를 다시 로드하지 않는다.
+	 */
 	@PostMapping("/board/writepro")
 	public String boardWritePro(Board board) {
 		
 		boardService.write(board);
 		
 		return "redirect:/";
+	}
+	
+	@GetMapping("/board/list")
+	public String boardList(Model model) {
+		
+		model.addAttribute("list", boardService.boardList());
+		
+		return "boardlist";
 	}
 }
