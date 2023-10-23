76c094701213108cd83fdfc77d605b1c5ccce4f4
Junku Kim <kimjunku0320@gmail.com>
2023-10-23 19:47:42 +0900

    Controller 추가: Comment Controller 추가
    Controller 추가 사항
     - ResponseEntity를 이용해 Http 상태코드를 전달하는 writeComment 메서드를 추가했습니다.
    
    기타 변경 사
     - 관련 이슈: #119
     Board Entity
       - 게시글과 댓글이 양방향일 필요가 없을 것 같아서 양방향 관계에서 단방향 관계로 바꿨습니다.
       - 추후 양방향 관계가 필요하다면 바꾸겠습니다.
     Comment Entity
       - Comment가 Oracle 환경에서 예약어이기 때문에 어노테이션을 통해 이름을 COMMENTS로 변경해주었습니다.
       - 자바 환경 내에서는 Comment로 유지됩니다.
       - Board 필드에도 not null 기능을 어노테이션으로 추가해줬습니다.
       - 연관관계를 설정해주는 setBoard 메서드를 추가했습니다.
     Comment Service
       - Board와 연관관계를 설정해주기 위해서  writeComment 메서드에 boardId 매개변수를 추가했습니다.
       - Board Service와는 다른 일은 한다고 생각돼서 Board Repository를 DI 받았습니다.

---
+++ board/src/main/java/com/jk/board/controller/CommentApiController.java
@@ -0,0 +1,40 @@
+package com.jk.board.controller;
+
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jk.board.dto.CommentRequest;
+import com.jk.board.dto.CommentResponse;
+import com.jk.board.service.CommentService;
+
+@RequestMapping("/api")
+@RestController
+public class CommentApiController {
+
+	private final CommentService commentService;
+	
+	public CommentApiController(final CommentService commentService) {
+		this.commentService = commentService;
+	}
+	
+	/*
+	 * 댓글 생성
+	 */
+	@PostMapping("/boards/{boardId}/comments")
+	public ResponseEntity<CommentResponse> writeComment(@PathVariable final Long boardId, @RequestBody final CommentRequest commentRequest) {
+		Long commentId = commentService.writeComment(boardId ,commentRequest);
+		Optional<CommentResponse> commentResponseOptional = commentService.findCommentById(commentId);
+		
+		if (commentResponseOptional.isPresent()) {
+			return ResponseEntity.ok(commentResponseOptional.get());
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+}
