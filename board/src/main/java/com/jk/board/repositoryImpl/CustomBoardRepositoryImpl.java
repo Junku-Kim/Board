43f4714a1520c3d035d135859d4d66b722c768b2
Junku Kim <kimjunku0320@gmail.com>
2023-11-24 19:26:02 +0900

    기능추가: 파일 다운로드 및 수정 기능 추가

--- board/src/main/java/com/jk/board/repositoryImpl/CustomBoardRepositoryImpl.java
+++ board/src/main/java/com/jk/board/repositoryImpl/CustomBoardRepositoryImpl.java
@@ -1,28 +1,53 @@
 package com.jk.board.repositoryImpl;
 
-import java.util.Collections;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
 import com.jk.board.dto.BoardFileRequest;
+import com.jk.board.entity.Board;
+import com.jk.board.exception.CustomException;
+import com.jk.board.exception.ErrorCode;
+import com.jk.board.repository.BoardRepository;
 import com.jk.board.repository.CustomBoardRepository;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 @Repository
 public class CustomBoardRepositoryImpl implements CustomBoardRepository {
 
 	@PersistenceContext
     private EntityManager entityManager;
 	
+	private final BoardRepository boardRepository;
+	
 	/*
 	 * 게시판 첨부파일 리스트
 	 */
 	@Override
 	public List<BoardFileRequest> selectBoardFileDetail(Long boardId) {
+		Board board = boardRepository.findById(boardId).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND));
+		String jpql = "SELECT NEW com.jk.board.dto.BoardFileRequest(" +
+					  "boardFile.originalName, " +
+					  "boardFile.savedName, " +
+					  "boardFile.uploadDir, " +
+					  "boardFile.extension, " +
+					  "boardFile.size, " +
+					  "boardFile.contentType, " +
+					  "boardFile.board) " +
+					  "FROM BoardFile boardFile " +
+					  "WHERE boardFile.board = :board " +
+					  "AND boardFile.isDeleted = :isDeleted";
 		
-		return Collections.emptyList();
+		List<BoardFileRequest> result = entityManager.createQuery(jpql, BoardFileRequest.class)
+				.setParameter("board", board)
+				.setParameter("isDeleted", false)
+				.getResultList();
+		
+		return result;
 	}
 }
