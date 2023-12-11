2c497075b17b1f1c34624714d93166ab2838e6c3
Junku Kim <kimjunku0320@gmail.com>
2023-12-11 17:44:33 +0900

    코드 리팩토링: 회원가입 및 로그인 기능 구현 전 전반적인 코드 리팩토링(Custom Board File Repo 상속)
    코드 리팩토링 사항
     Board File API Controller
      - CustomBoardFileRepository를 제거하고 BoardFileService를 통해 처리하도록 변경했습니다.
    
     Board File Repository
      - CustomBoardFileRepository 인터페이스를 상속하는 부분을 추가했습니다.
    
     Board File Service
      - CustomBoardFileRepsoitry 상속을 통해 받아 온 메서드를 사용하는 메서드를 추가했습니다.
    
     Board View API Controller
      - CustomBoardFileRepository가 아닌 BoardFileService를 통해 값을 처리하도록 변경했습니다.
    
     관련 이슈
      - #204
    
    테스트 환경
     - **웹 사이트 테스트 환경**

--- board/src/main/java/com/jk/board/repository/BoardFileRepository.java
+++ board/src/main/java/com/jk/board/repository/BoardFileRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jk.board.entity.BoardFile;
 
-public interface BoardFileRepository extends JpaRepository<BoardFile, Long> {
+public interface BoardFileRepository extends JpaRepository<BoardFile, Long>, CustomBoardFileRepository {
 
 }
