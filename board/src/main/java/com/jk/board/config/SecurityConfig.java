9609ead2f41424afb5c68490aa7c2df94794c5a0
Junku Kim <kimjunku0320@gmail.com>
2023-12-17 15:56:44 +0900

    기능 추가: 로그인 기능 추가 (csrf deprecated 문제 해결)
    기능 추가 사항
     Security Config
      - deprecated된 csrf가 아닌 다른 csrf를 통해 CSRF기능을 비활성화 했습니다.
        - RESTful API를 통해 처리할 경우 무상태성이기 때문에 비활성화 해도 문제가 없을 수도 있다고 합니다. 지금은
    공부중이니 우선 비활성화하고 추후 개발하면서 다시 확인하겠습니다.
    
    테스트 환경
     - **웹 사이트 환경**
    
    기타 수정 사항
     Member API Controller
      - 생성자 DI를 어노테이션을 통해 DI 하는 것으로 변경했습니다.

--- board/src/main/java/com/jk/board/config/SecurityConfig.java
+++ board/src/main/java/com/jk/board/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -26,11 +27,11 @@ public class SecurityConfig {
 		httpSecurity.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
 				.requestMatchers(new AntPathRequestMatcher("/**")).permitAll());
 		
-		return httpSecurity.csrf().disable().build();
+		return httpSecurity.csrf(AbstractHttpConfigurer::disable).build();
 	}
 	
 	@Bean
-	PasswordEncoder passwordEncoder() {
+	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
